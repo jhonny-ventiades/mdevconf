a3efc7713b58e992340e0de0af8ebc891dc4ecb8
Jhonny Ventiades Guillen <jhonny.ventiadesg@gmail.com>
2024-11-15 18:16:38 -0400

    update package name

--- android/app/src/main/java/com/jv/mdevconf24/MainActivity.java
+++ android/app/src/main/java/com/jv/mdevconf24/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.jv.mdevconf24;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.jv.mdevconf24',
   appName: 'mDevConf',
   webDir: 'www'
 };
